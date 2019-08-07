fceb0e28069256ee9d88f9f1e04b0f2e0ef38379
Naoki Matsumoto <m.naoki9911@gmail.com>
2019-08-07 13:08:18 +0900

    Testing

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ uint16_t pc_read(struct cpu *c){
 
 void reg_write(struct cpu *c, uint8_t reg_idx, uint16_t data){
     c->reg[reg_idx] = data;
-    printf("Reg x%d <= 0x%04X", reg_idx, data);
+    printf("Reg x%d <= 0x%04X\n", reg_idx, data);
 }
 
 uint16_t reg_read(struct cpu *c, uint8_t reg_idx){
@@ -28,14 +28,14 @@ uint16_t reg_read(struct cpu *c, uint8_t reg_idx){
 
 void mem_write_b(struct cpu *c, uint16_t addr, uint8_t data){
     c->data_ram[addr] = data;
-    printf("DataRam[0x%04X] <= 0x%04X", addr, data);
+    printf("DataRam[0x%04X] <= 0x%04X\n", addr, data);
 }
 
 void mem_write_w(struct cpu *c, uint16_t addr, uint16_t data){
     c->data_ram[addr] = data&0xFF;
     c->data_ram[addr+1] = data>>8;
-    printf("DataRam[0x%04X] <= 0x%04X", addr, data&0xFF);
-    printf("DataRam[0x%04X] <= 0x%04X", addr, data>>8);
+    printf("DataRam[0x%04X] <= 0x%04X\n", addr, data&0xFF);
+    printf("DataRam[0x%04X] <= 0x%04X\n", addr, data>>8);
 }
 
 uint8_t mem_read_b(struct cpu *c, uint16_t addr){
@@ -102,12 +102,16 @@ void init_cpu(struct cpu *c){
     c->flag_overflow = 0;
     c->flag_zero = 0;
     c->flag_carry = 0;
+    c->inst_rom[0] = 0x08;
+    c->inst_rom[1] = 0x78;
+    c->inst_rom[2] = 0xFF;
+    c->inst_rom[3] = 0xFF;
 }
 
 int main(char* argv[], int argc){
     struct cpu cpu;
     init_cpu(&cpu);
-    elf_parse(&cpu, argv[1]);
+    //elf_parse(&cpu, argv[1]);
 
     uint16_t inst = rom_read_w(&cpu);
     uint8_t rs = get_bits(inst, 4, 7);
@@ -121,6 +125,7 @@ int main(char* argv[], int argc){
     switch(get_bits(inst, 14, 15)){
         case 0b00:
             //NOP
+            printf("Inst:NOP\n");
             pc_update(&cpu, 2);
             cpu.flag_sign = 0;
             cpu.flag_overflow = 0;
@@ -136,12 +141,14 @@ int main(char* argv[], int argc){
             switch(get_bits(inst, 8, 12)){
                 case 0b10010:
                     //J
+                    printf("Inst:J\n");
                     pc_update(&cpu, 2);
                     imm = rom_read_w(&cpu);
                     pc_update(&cpu, imm-2);
                     break;
                 case 0b10011:
                     //JAL
+                    printf("Inst:JAL\n");
                     reg_write(&cpu, 0, pc_read(&cpu));
                     pc_update(&cpu, 2);
                     imm = rom_read_w(&cpu);
@@ -149,19 +156,23 @@ int main(char* argv[], int argc){
                     break;
                 case 0b00001:
                     //JALR
+                    printf("Inst:JALR\n");
                     reg_write(&cpu, 0, pc_read(&cpu));
                     pc_write(&cpu, reg_read(&cpu, rs));
                     break;
                 case 0b00000:
                     //JR
+                    printf("Inst:JR\n");
                     pc_write(&cpu, reg_read(&cpu, rs));
                     break;
                 case 0b00100:
                     //JL,JLE
                     imm = sign_ext(get_bits(inst, 0, 6), 6);
                     if(get_bits(inst, 7, 7) == 0 && cpu.flag_sign != cpu.flag_overflow){
+                        printf("Inst:JL\n");
                         pc_update(&cpu, imm);
                     }else if(get_bits(inst, 7, 7) == 1 && (cpu.flag_sign != cpu.flag_overflow || cpu.flag_zero == 1)){
+                        printf("Inst:JLE\n");
                         pc_update(&cpu, imm);
                     }else{
                         pc_update(&cpu, 2);
@@ -171,8 +182,10 @@ int main(char* argv[], int argc){
                     //JE,LNE
                     imm = sign_ext(get_bits(inst, 0, 6), 6);
                     if(get_bits(inst, 7, 7) == 0 && cpu.flag_zero == 1){
+                        printf("Inst:JE\n");
                         pc_update(&cpu, imm);
                     }else if(get_bits(inst, 7, 7) == 1 && cpu.flag_zero == 0){
+                        printf("Inst:JNE\n");
                         pc_update(&cpu, imm);
                     }else{
                         pc_update(&cpu, 2);
@@ -182,8 +195,10 @@ int main(char* argv[], int argc){
                     //JB,JBE
                     imm = sign_ext(get_bits(inst, 0, 6), 6);
                     if(get_bits(inst, 7, 7) == 0 && cpu.flag_carry == 1){
+                        printf("Inst:JB\n");
                         pc_update(&cpu, imm);
                     }else if(get_bits(inst, 7, 7) == 1 && (cpu.flag_carry == 1 || cpu.flag_zero == 1)){
+                        printf("Inst:JBE\n");
                         pc_update(&cpu, imm);
                     }else{
                         pc_update(&cpu, 2);
@@ -191,6 +206,7 @@ int main(char* argv[], int argc){
                     break;
                 case 0b11000:
                     //LI
+                    printf("Inst:LI\n");
                     pc_update(&cpu, 2);
                     imm = rom_read_w(&cpu);
                     cpu.flag_carry = 0;
@@ -207,6 +223,7 @@ int main(char* argv[], int argc){
             switch(get_bits(inst, 12, 13)){
                 case 0b00:
                     //SWSP
+                    printf("Inst:SWSP\n");
                     imm = (get_bits(inst, 8, 11)<<5)+(rs<<1);
                     s_data = reg_read(&cpu, 1);
                     res = s_data+imm;
@@ -237,15 +254,18 @@ int main(char* argv[], int argc){
                     cpu.flag_zero = flag_zero(res&0xFFFF);
                     if(get_bits(inst, 11, 11)){
                         //SW
+                        printf("Inst:SW\n");
                         mem_write_w(&cpu, res&0xFFFF, reg_read(&cpu, rs));
                     }else{
                         //SB
+                        printf("Inst:SB\n");
                         mem_write_b(&cpu, res&0xFFFF, reg_read(&cpu, rs)&0xFF);
                     }
                     pc_update(&cpu, 2);
                     break;
                 case 0b10:
                     //LWSP
+                    printf("Inst:LWSP\n");
                     imm = (get_bits(inst, 4, 11)<<1);
                     d_data = reg_read(&cpu, 1);
                     res = imm+d_data;
@@ -277,14 +297,17 @@ int main(char* argv[], int argc){
                     switch(get_bits(inst, 10, 11)){
                         case 0b00:
                             //LW
+                            printf("Inst:LW\n");
                             reg_write(&cpu, rd, mem_read_w(&cpu, res&0xFFFF));
                             break;
                         case 0b10:
                             //LBU
+                            printf("Inst:LBU\n");
                             reg_write(&cpu, rd, mem_read_b(&cpu, res&0xFFFF));
                             break;
                         case 0b11:
                             //LB
+                            printf("Inst:LB\n");
                             reg_write(&cpu, rd, sign_ext(mem_read_b(&cpu, res&0xFFFF), 7));
                             break;
                     }
@@ -297,6 +320,7 @@ int main(char* argv[], int argc){
             switch(get_bits(inst, 8, 11)){
                 case 0b0000:
                     //MOV
+                    printf("Inst:MOV\n");
                     s_data = reg_read(&cpu, rs);
                     reg_write(&cpu, rd, s_data);
                     pc_update(&cpu, 2);
@@ -309,6 +333,7 @@ int main(char* argv[], int argc){
                     //ADD[I]
                     if(get_bits(inst, 12, 12) == 0){
                         //ADD
+                        printf("Inst:ADD\n");
                         s_data = reg_read(&cpu, rs);
                         d_data = reg_read(&cpu, rd);
                         uint32_t res = s_data+d_data;
@@ -323,6 +348,7 @@ int main(char* argv[], int argc){
                         reg_write(&cpu, rd, res&0xFFFF);
                     }else{
                         //ADDI
+                        printf("Inst:ADDI\n");
                         s_data = sign_ext(rs, 3);
                         d_data = reg_read(&cpu, rd);
                         uint32_t res = s_data+d_data;
@@ -339,9 +365,10 @@ int main(char* argv[], int argc){
                     pc_update(&cpu, 2);
                     break;
                 case 0b0011:
-                    //SUB,CMPI
+                    //SUB,CMP
                     if(get_bits(inst, 12, 12) == 0){
                         //SUB
+                        printf("Inst:SUB\n");
                         s_data = (~reg_read(&cpu, rs))+1;
                         d_data = reg_read(&cpu, rd);
                         uint32_t res = s_data+d_data;
@@ -355,7 +382,8 @@ int main(char* argv[], int argc){
                         cpu.flag_zero = flag_zero(res&0xFFFF);
                         reg_write(&cpu, rd, res&0xFFFF);
                     }else{
-                        //CMPI
+                        //CMP
+                        printf("Inst:CMP\n");
                         s_data = (~sign_ext(rs, 3))+1;
                         d_data = reg_read(&cpu, rd);
                         uint32_t res = s_data+d_data;
@@ -372,6 +400,7 @@ int main(char* argv[], int argc){
                     break;
                 case 0b0100:
                     //AND
+                    printf("Inst:AND\n");
                     s_data = reg_read(&cpu, rs);
                     d_data = reg_read(&cpu, rd);
                     res_w = s_data&d_data;
@@ -384,6 +413,7 @@ int main(char* argv[], int argc){
                     break;
                 case 0b0101:
                     //OR
+                    printf("Inst:OR\n");
                     s_data = reg_read(&cpu, rs);
                     d_data = reg_read(&cpu, rd);
                     res_w = s_data|d_data;
@@ -396,6 +426,7 @@ int main(char* argv[], int argc){
                     break;
                 case 0b0110:
                     //XOR
+                    printf("Inst:XOR\n");
                     s_data = reg_read(&cpu, rs);
                     d_data = reg_read(&cpu, rd);
                     res_w = s_data|d_data;
@@ -408,6 +439,7 @@ int main(char* argv[], int argc){
                     break;
                 case 0b1001:
                     //LSL
+                    printf("Inst:LSL\n");
                     s_data = reg_read(&cpu, rs);
                     d_data = reg_read(&cpu, rd);
                     res_w = d_data << s_data;
@@ -420,6 +452,7 @@ int main(char* argv[], int argc){
                     break;
                 case 0b1010:
                     //LSR
+                    printf("Inst:LSR\n");
                     s_data = reg_read(&cpu, rs);
                     d_data = reg_read(&cpu, rd);
                     res_w = d_data >> s_data;
@@ -432,6 +465,7 @@ int main(char* argv[], int argc){
                     break;
                 case 0b1100:
                     //ASR
+                    printf("Inst:ASR\n");
                     s_data = reg_read(&cpu, rs);
                     d_data = reg_read(&cpu, rd);
                     res_w = ((int16_t)d_data) >> s_data;
